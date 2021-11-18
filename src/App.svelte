11997cc92f753c17573b67858b4bd81ce89da95b
jeroenstolk <info@jeroen.dk>
2021-11-18 15:42:36 +0100

    get beers

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
+	import Beers from "./beers.svelte";
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Beers />
+
 </main>
 
 <style>
---
+++ src/beers.svelte
@@ -0,0 +1,19 @@
+<script>
+    const fetchBeers = (async () => {
+        const response = await fetch("https://api.punkapi.com/v2/beers");
+        return await response.json();
+    })();
+    
+</script>
+
+
+{#await fetchBeers}
+    <p>Loading beers...</p>
+{:then beers}
+    {#each beers as beer}
+        <img src={beer.image_url} alt={beer.name} />
+    {/each}
+{/await}
+
+<style>
+</style>
